c315d7d7bc69531276d9cbe3a4f87e9d7ee084fa
Muruga <davanon2014@gmail.com>
2021-10-07 10:54:21 +0530

    First

--- src/main/java/com/iqmsoft/HazelcastConfiguration.java
+++ src/main/java/com/iqmsoft/HazelcastConfiguration.java
@@ -1,9 +1,10 @@
 package com.iqmsoft;
 
 import com.hazelcast.config.Config;
-import com.hazelcast.config.EvictionPolicy;
+//import com.hazelcast.config.EvictionPolicy;
 import com.hazelcast.config.MapConfig;
-import com.hazelcast.config.MaxSizeConfig;
+//import com.hazelcast.config.MaxSizeConfig;
+//import com.hazelcast.config.MaxSizePolicy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,8 +18,8 @@ public class HazelcastConfiguration {
                 .addMapConfig(
                         new MapConfig()
                                 .setName("instruments")
-                                 .setMaxSizeConfig(new MaxSizeConfig(200, MaxSizeConfig.MaxSizePolicy.FREE_HEAP_SIZE))
-                                .setEvictionPolicy(EvictionPolicy.LRU)
+                               // .setMaxSizeConfig(new MaxSizeConfig(200, MaxSizeConfig.MaxSizePolicy.FREE_HEAP_SIZE))
+                                //.setEvictionPolicy(EvictionPolicy.LRU)
                                 .setTimeToLiveSeconds(20));
     }
 
